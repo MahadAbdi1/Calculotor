30a2c7dc0f07f7a2e34dda8e0671b18cb3d68f2c
Mahad Hassan <fantasticcr2@gmail.com>
2021-03-30 14:51:32 +0200

    first commit on my calculator method

---
+++ src/za/ac/cput/calculotor/Subtract.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package za.ac.cput.calculotor;
+
+/**
+ *
+ * @author Mahad
+ */
+public class Subtract {
+    
+    public static int  intergerSub(int a , int b ){
+        
+        return a -b ;
+        
+    }
+    
+}
